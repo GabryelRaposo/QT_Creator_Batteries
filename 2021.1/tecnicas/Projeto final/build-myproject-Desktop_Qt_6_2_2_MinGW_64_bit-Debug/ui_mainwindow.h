cf879d7276264ef656f40e6c8a90085c19b55d40
Gabryel Raposo <gabryelraposo@gmail.com>
2021-12-12 12:08:24 -0300

    add graphic visualization

--- 2021.1/tecnicas/Projeto final/build-myproject-Desktop_Qt_6_2_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ 2021.1/tecnicas/Projeto final/build-myproject-Desktop_Qt_6_2_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -9,6 +9,7 @@
 #ifndef UI_MAINWINDOW_H
 #define UI_MAINWINDOW_H
 
+#include <QtCharts/QChartView>
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
@@ -33,6 +34,10 @@ public:
     QLabel *label_2;
     QLineEdit *lineEdit_2;
     QPushButton *pushButton_2;
+    QChartView *graphicsView;
+    QChartView *graphicsView_2;
+    QChartView *graphicsView_3;
+    QChartView *graphicsView_4;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -40,34 +45,46 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(782, 450);
+        MainWindow->resize(1327, 696);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(30, 20, 49, 16));
+        label->setGeometry(QRect(40, 60, 91, 16));
         textEdit = new QTextEdit(centralwidget);
         textEdit->setObjectName(QString::fromUtf8("textEdit"));
-        textEdit->setGeometry(QRect(30, 50, 231, 271));
+        textEdit->setGeometry(QRect(20, 120, 271, 271));
         lineEdit = new QLineEdit(centralwidget);
         lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
-        lineEdit->setGeometry(QRect(60, 20, 113, 22));
+        lineEdit->setGeometry(QRect(140, 60, 151, 22));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(180, 20, 75, 24));
+        pushButton->setGeometry(QRect(20, 410, 271, 24));
         label_2 = new QLabel(centralwidget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(40, 330, 51, 16));
+        label_2->setGeometry(QRect(60, 20, 51, 16));
         lineEdit_2 = new QLineEdit(centralwidget);
         lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
-        lineEdit_2->setGeometry(QRect(110, 330, 151, 22));
+        lineEdit_2->setGeometry(QRect(140, 20, 151, 22));
         pushButton_2 = new QPushButton(centralwidget);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(30, 360, 231, 24));
+        pushButton_2->setGeometry(QRect(20, 90, 271, 24));
+        graphicsView = new QChartView(centralwidget);
+        graphicsView->setObjectName(QString::fromUtf8("graphicsView"));
+        graphicsView->setGeometry(QRect(310, 20, 481, 301));
+        graphicsView_2 = new QChartView(centralwidget);
+        graphicsView_2->setObjectName(QString::fromUtf8("graphicsView_2"));
+        graphicsView_2->setGeometry(QRect(310, 340, 481, 301));
+        graphicsView_3 = new QChartView(centralwidget);
+        graphicsView_3->setObjectName(QString::fromUtf8("graphicsView_3"));
+        graphicsView_3->setGeometry(QRect(800, 20, 481, 301));
+        graphicsView_4 = new QChartView(centralwidget);
+        graphicsView_4->setObjectName(QString::fromUtf8("graphicsView_4"));
+        graphicsView_4->setGeometry(QRect(800, 340, 481, 301));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 782, 22));
+        menubar->setGeometry(QRect(0, 0, 1327, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -81,8 +98,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        label->setText(QCoreApplication::translate("MainWindow", "UFPB", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Number of lines", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Draw Graphic", nullptr));
         label_2->setText(QCoreApplication::translate("MainWindow", "CSV FILE", nullptr));
         pushButton_2->setText(QCoreApplication::translate("MainWindow", "Open File", nullptr));
     } // retranslateUi
--- 2021.1/tecnicas/Projeto final/myproject/mainwindow.cpp
+++ 2021.1/tecnicas/Projeto final/myproject/mainwindow.cpp
@@ -3,6 +3,9 @@
 #include <QFile>
 #include <QWidget>
 #include <QLabel>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QSplineSeries>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -20,21 +23,101 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-kkkkkkkkk
+    double value;
+    QChart *chart, *chart1, *chart2, *chart3;
+    QChartView *chartView, *chartView1, *chartView2, *chartView3;
+    QSplineSeries *seriesA = new QSplineSeries(); // Alocando memória para a série SEN
+    seriesA->setName("Corrente"); // setado o nome da série
+    QSplineSeries *seriesB = new QSplineSeries(); // Alocando memória para a série COSSENO
+    seriesB->setName("Temperatura Ambiente"); // setado o nome da série
+    QSplineSeries *seriesC = new QSplineSeries(); // Alocando memória para a série COSSENO
+    seriesB->setName("Tensão Bateria"); // setado o nome da série
+    QSplineSeries *seriesD = new QSplineSeries(); // Alocando memória para a série COSSENO
+    seriesB->setName("Temperatura Bateria"); // setado o nome da série
+    QFile inputFile(ui->lineEdit_2->text());
+    QString XMAX=ui->lineEdit->text();
+    value = XMAX.toDouble();
+    if(!inputFile.open(QIODevice::ReadOnly | QIODevice::Text)) qDebug() << "Arquivo não aberto"; else qDebug() << "Arquivo OK";
+    QTextStream in(&inputFile);
+    QString line = in.readLine(); // Ler primeira linha e descarta
+    float x, y, z,j; char lixo; int k;
+    for (k=0;k<value;k++) {
+    in >> x;
+    in >> lixo; // ler 1a ','
+    in >> y;
+    in >> lixo; // ler 2a ','
+    in >> z;
+    in >> lixo; // ler 3a ','
+    in >> j;
+    *seriesA << QPointF(k, x);
+    *seriesB << QPointF(k, y);
+    *seriesC << QPointF(k, z);
+    *seriesD << QPointF(k, j);
+    }
+    inputFile.close();
+    seriesA->setName("Corrente");
+    seriesB->setName("Temperatura Ambiente");
+    seriesC->setName("Tensão");
+    seriesD->setName("Temperatura Bateria");
+    chart = new QChart(); //Criando um gráfico CHART
+    chart1 = new QChart();
+    chart2 = new QChart();
+    chart3 = new QChart();
+    chart->addSeries(seriesA); //Adiconando cada série no gráfico
+    chart->legend()->setVisible(true);
+    chart->setTitle("Grafico Corrente");
+    chart->createDefaultAxes();
+
+    chart1->addSeries(seriesB);
+    chart1->legend()->setVisible(true);
+    chart1->setTitle("Grafico Temperatura Ambiente");
+    chart1->createDefaultAxes();
+
+    chart2->addSeries(seriesC);
+    chart2->legend()->setVisible(true);
+    chart2->setTitle("Grafico Tensão");
+    chart2->createDefaultAxes();
+
+    chart3->addSeries(seriesD);
+    chart3->legend()->setVisible(true);
+    chart3->setTitle("Grafico Temperatura Bateria");
+    chart3->createDefaultAxes();
+
+    chartView = new QChartView(chart); //Criando o visualizador do gráfico CHART
+    chartView1 = new QChartView(chart1); //Criando o visualizador do gráfico CHART
+    chartView2 = new QChartView(chart2); //Criando o visualizador do gráfico CHART
+    chartView3 = new QChartView(chart3); //Criando o visualizador do gráfico CHART
+    chartView->setRenderHint(QPainter::Antialiasing); //"Amaciar as linhas (evitar degraus)
+    ui->graphicsView->setChart(chart);
+    ui->graphicsView_2->setChart(chart1);
+    ui->graphicsView_3->setChart(chart2);
+    ui->graphicsView_4->setChart(chart3);
 }
 
 
 void MainWindow::on_pushButton_2_clicked()
 {
+    double value; int x;
 QFile inputFile(ui->lineEdit_2->text());
 if(!inputFile.open(QIODevice::ReadOnly))
 qDebug() << "Arquivo não aberto";
 else
 qDebug() << "Arquivo OK";
+QString XMAX=ui->lineEdit->text();
+value = XMAX.toDouble();
 QTextStream in(&inputFile);
-QString line = in.readAll();
+QString line = in.readLine();
+ui->textEdit->setText(line);
+for (x=0;x<value;x++) {
+    QString line = in.readLine();
+    ui->textEdit->append(line);
+    qDebug() << line;
+
+}
+
 inputFile.close();
-ui->textEdit->setPlainText(line);
+qDebug() << value;
+
 }
 
 
